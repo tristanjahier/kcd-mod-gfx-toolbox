e0540c73ef53eb5a86d5681211f489fb11d054e0
Tristan Jahier <tristan.jahier@gmail.com>
2026-02-25 19:06:22 +0100

    Add output dir arg to extract command

--- src/kcd_gfx_toolbox/cli_extract.py
+++ src/kcd_gfx_toolbox/cli_extract.py
@@ -10,13 +10,19 @@ from .utils import AnsiColor, ensure_empty_dir, get_temp_dir, print_error
 
 def command(
     input_file: Annotated[Path, typer.Argument(help="The GFx file to extract.")],
+    output_dir: Annotated[
+        Path | None,
+        typer.Argument(
+            help="Destination directory for extracted files. Any existing contents will be deleted. If omitted, a temporary directory is used."
+        ),
+    ] = None,
     ffdec_path: Annotated[
         Path | None,
         typer.Option("--ffdec", help="Path to the ffdec binary. Only required if it is not in the system PATH."),
     ] = None,
 ):
     """
-    Extract scripts from a GFx file into a temporary directory.
+    Extract scripts from a GFx file into a directory.
     """
     input_file = input_file.resolve()
 
@@ -24,13 +30,19 @@ def command(
         print_error(f"Invalid input: {input_file} does not exist or is not a file.")
         raise typer.Exit(code=1)
 
-    temp_dir = get_temp_dir()
+    if output_dir is None:
+        temp_dir = get_temp_dir()
 
-    if temp_dir.exists() and not temp_dir.is_dir():
-        print_error(f"Temp path exists but is not a directory: {temp_dir}")
-        raise typer.Exit(code=1)
+        if temp_dir.exists() and not temp_dir.is_dir():
+            print_error(f"Temp path exists but is not a directory: {temp_dir}")
+            raise typer.Exit(code=1)
 
-    temp_dir.mkdir(parents=True, exist_ok=True)
+        temp_dir.mkdir(parents=True, exist_ok=True)
+
+        input_file_hash = extraction_cache_key(input_file)
+        output_dir = (temp_dir / f"{input_file.stem}_{input_file_hash}" / "raw").resolve()
+    else:
+        output_dir = output_dir.resolve()
 
     try:
         ffdec_path = resolve_ffdec(ffdec_path)
@@ -39,9 +51,6 @@ def command(
         raise typer.Exit(code=1)
 
     print(f"Using ffdec at {ffdec_path}")
-
-    input_file_hash = extraction_cache_key(input_file)
-    output_dir = (temp_dir / f"{input_file.stem}_{input_file_hash}" / "raw").resolve()
 
     print(f"Extracting {input_file.name} contents in: {output_dir}")
 
